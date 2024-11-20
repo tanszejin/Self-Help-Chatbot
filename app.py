d2ed8b38a632f0042ecdd40b733d83d0fcd778da
tanszejin <160217134+tanszejin@users.noreply.github.com>
2024-11-20 12:10:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,19 +4,26 @@ from rag_llm import RAG_LLM
 
 st.title("Self-Help Chatbot")
 
-chatbot = RAG_LLM()
+# get api key from user
+groq_api_key = ""
+groq_url = "https://console.groq.com/keys"
+st.write("First get a Groq API Key from [here](%s). Don't worry, it's free!" % groq_url)
+groq_api_key = st.text_input("Groq API Key", type="password")
 
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+if len(groq_api_key) > 0:
+    chatbot = RAG_LLM(groq_api_key)
 
-for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).markdown(msg["content"])
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
 
-query = st.chat_input("Ask away!")
+    for msg in st.session_state.messages:
+        st.chat_message(msg["role"]).markdown(msg["content"])
 
-if query:
-    st.chat_message("user").markdown(query)
-    st.session_state.messages.append({"role": "user", "content":query})
-    response = chatbot.ask(query)
-    st.chat_message('assistant').markdown(response)
-    st.session_state.messages.append({"role": "assistant", "content":response})
+    query = st.chat_input("Ask away!")
+
+    if query:
+        st.chat_message("user").markdown(query)
+        st.session_state.messages.append({"role": "user", "content":query})
+        response = chatbot.ask(query)
+        st.chat_message('assistant').markdown(response)
+        st.session_state.messages.append({"role": "assistant", "content":response})
